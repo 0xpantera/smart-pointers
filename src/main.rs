fd2587642ed8095663d18ac5f54d4e8041f63721
Francososa <franky.sosa@gmail.com>
2022-04-06 14:08:00 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use crate::List::{Cons, Nil};
+use std::{ops::Deref, rc::Rc, cell::RefCell};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Nil => None,
+            Cons(x, xs) => Some(xs),
+        }
+    }
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("hello {}", name);
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    println!("a next item = {:?}", a.tail());
+    
+    let test = Rc::new(RefCell::new(5));
+    println!("test val: {:?}", test);
+    
+    *test.borrow_mut() += 10;
+    println!("test val after: {:?}", test);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+
+    let _c = CustomSmartPointer { data: String::from("my stuff") };
+    let _d = CustomSmartPointer { data: String::from("other stuff") };
+    println!("CustomSmartPointers created.");
+
+
+}
